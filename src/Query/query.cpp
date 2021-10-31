bb7af86b08eeebf0b1cd5e30ca5832145daf3fd3
Clotilde LEVESQUE <clotilde.levesque@epita.fr>
2021-11-01 00:51:40 +0100

    [WIP] Query build apod

--- src/Query/query.cpp
+++ src/Query/query.cpp
@@ -1,1 +1,14 @@
 #include "query.hpp"
+
+std::string Query::buildApod(bool thumb, std::string date = "today", bool conceptTags = false, bool hd = true) {
+    std::stringstream ss;
+    ss << "https://api.nasa.gov/planetary/apod?";
+}
+
+std::string Query::buildApod(std::string startDate, std::string endDate, bool conceptTags, bool hd, bool thumb) {
+    return std::string();
+}
+
+std::string Query::buildApod(int count, bool conceptTags, bool hd, bool thumb) {
+    return std::string();
+}
--- src/Query/query.hpp
+++ src/Query/query.hpp
@@ -1,6 +1,38 @@
 #pragma once
 
+#include <string>
+#include <sstream>
+
+enum QueryType
+{
+    APOD,
+    ANW,
+    DONKI,
+    EARTH,
+    EONET,
+    EPIC,
+    EXOPLANET,
+    GENELAB,
+    INSIGHT,
+    MRP,
+    NIVL,
+    TECHTRANS,
+    SSC,
+    SSDCNEOS,
+    TECHPORT,
+    VMMT
+};
+
 class Query {
+private:
+    std::string apiKey;
+    enum QueryType type;
+
+    std::string buildApod(bool thumb, std::string date = "today", bool conceptTags = false, bool hd = true);
+    std::string buildApod(std::string startDate, std::string endDate, bool thumb, bool conceptTags = false, bool hd = true);
+    std::string buildApod(int count, bool thumb, bool conceptTags = false, bool hd = true);
+
+public:
 
 };
 
