19ccca26577f6b80891c98fa56081f3e562eac67
Clotilde LEVESQUE <clotilde.levesque@epita.fr>
2021-10-31 21:24:13 +0100

    [FEAT] Basic Logger

---
+++ src/Logger/logger.cpp
@@ -0,0 +1,30 @@
+#include "logger.hpp"
+
+enum LoggerStatus Logger::getStatus() {
+    return this->status;
+}
+
+void Logger::setStatus(enum LoggerStatus newStatus) {
+    this->status = newStatus;
+}
+
+void Logger::printLog(std::string &log) {
+    std::string color;
+    std::string header = "[INFO]";
+    std::string reset = "\033[0m";
+    std::time_t t = std::time(nullptr);
+    std::tm tm = *std::localtime(&t);
+
+    if (this->status == OK)
+        color = "\033[1;32m";
+    else {
+        color = "\033[1;31m";
+        header = "[ERROR]";
+    }
+
+    std::stringstream ss;
+    ss << color << header << "[" << std::put_time(&tm, "%a %b %d %H:%M:%S %Y") << "] " << reset << log << "\n";
+    std::cout << ss.str();
+}
+
+
---
+++ src/Logger/logger.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+
+enum LoggerStatus
+{
+    ERROR,
+    OK
+};
+
+class Logger {
+private:
+    enum LoggerStatus status;
+
+public:
+
+    Logger():
+    status(OK)
+    {}
+
+    enum LoggerStatus getStatus();
+
+    void setStatus(enum LoggerStatus newStatus);
+
+    void printLog(std::string &log);
+};
+
+
+
