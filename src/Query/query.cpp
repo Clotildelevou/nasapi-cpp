ba9510a63dab7c78dde1c7380954ec1db021d4ab
Clotilde LEVESQUE <clotilde.levesque@epita.fr>
2021-11-04 16:52:55 +0100

    [FIX] APOD corrected

--- src/Query/query.cpp
+++ src/Query/query.cpp
@@ -20,25 +20,27 @@ void Query::Apod(const std::string& date, bool thumb) {
 void Query::Apod(const std::string &startDate, const std::string &endDate, bool thumb) {
     std::stringstream ss;
 
-    ss << "GET / https://api.nasa.gov/planetary/apod?api_key=" << this->apiKey
+    ss << "GET /planetary/apod?api_key=" << this->apiKey
     << "&start_date=" << startDate
     << "&end_date=" << endDate;
 
     if (thumb)
         ss << "&thumbs=true";
 
-    ss << " HTTP/1.0"
-    << "\\r\\n\\r\\n";;
+    ss << " HTTP/1.1\r\n"
+    << "Host: api.nasa.gov"
+    << "\r\n\r\n";;
     this->query = ss.str();
 }
 
 void Query::Apod(int count, bool thumb) {
     std::stringstream ss;
 
-    ss << "GET / https://api.nasa.gov/planetary/apod?api_key=" << this->apiKey
+    ss << "GET /planetary/apod?api_key=" << this->apiKey
     << "&count=" << count
-    << " HTTP/1.0"
-    << "\\r\\n\\r\\n";
+    << " HTTP/1.0\r\n"
+    << "Host: api.nasa.gov"
+    << "\r\n\r\n";
     this->query = ss.str();
 }
 
