a3cb6ace9934509e7b209f17419304e86aa861c7
Clotilde LEVESQUE <clotilde.levesque@epita.fr>
2021-11-04 09:56:05 +0100

    [DOCUMENTATION] First step Doxygen

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
+#include "query.hpp"
+#include "client.hpp"
+
 
 int main(void)
 {
-
+    Client client = Client();
+    std::string apiKey = "byBP90bGzkOdifclHdWVeUecgGg8oj9IoxnTLqW1";
+    client.Apod(apiKey);
 }
